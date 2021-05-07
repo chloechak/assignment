51ffaa3f7f486cd78cc42e1e12b6ed93e94247a8
lakysha123 <81129046+lakysha123@users.noreply.github.com>
2021-05-07 18:21:08 -0400

    added imports

--- benfordlaw.java
+++ benfordlaw.java
@@ -2,3 +2,16 @@
 // hello
 //new change:)
 //another change
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+/**
+ * benfordlaw
+ */
+class benfordlaw {
+
+    
+}
