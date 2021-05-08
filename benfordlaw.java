3553bef7bbcc9e17d97fd685ec9f6e8e00a0b6b5
lakysha123 <81129046+lakysha123@users.noreply.github.com>
2021-05-08 10:20:47 -0400

    added 2 counters

--- benfordlaw.java
+++ benfordlaw.java
@@ -2,3 +2,22 @@
 // hello
 //new change:)
 //another change
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+/**
+ * benfordlaw
+ */
+class benfordlaw {
+    public static void main(String[] args) throws Exception{
+        int i=0;
+        int t=0;
+       // The file to be read by scanner
+       File file = new File("/Users/lakysha/Downloads/sales.csv");
+       Scanner sc = new Scanner(file);
+    }
+    
+}
