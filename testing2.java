277537b8fd001a5a8a136d0766f0065a06d220ad
chloechak <chloechak0@gmail.com>
2021-05-09 13:20:53 -0400

    i finally did it

--- testing2.java
+++ testing2.java
@@ -10,6 +10,8 @@ import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 
 public class testing2 extends Application{ 
+    double[] countArray = new double[10];     
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -19,9 +21,9 @@ public class testing2 extends Application{
        //this scanner is names scan and it stores the csv file required for the benford calculation
         Scanner scan = new Scanner(new File("sales (2).csv"));
         //this array stores the numbers from 1-9 (10 numbers)
-        double[] countArray = new double[10];     
+        //double[] countArray = new double[10];     
         //initialize the counter of the numbers appearing
-        int appearingNum = 0; 
+        double appearingNum = 0.0; 
         //while the file has next perform the tasks 
         while (scan.hasNext()) {
             //read the next digit but convert the 4th character into an int 
@@ -39,15 +41,16 @@ public class testing2 extends Application{
         //display the method that contains the final percentages
         finalPercent(countArray,appearingNum); 
     }
-    public void finalPercent(double [] countArray, int appearingNum) {
+    public void finalPercent(double [] countArray, double appearingNum) {
         // this for loop will print out the frequency of every number from 1-9
         for (int i = 1; i<=9; i++){
             countArray[i] = countArray[i] * 100.0 / appearingNum;
             System.out.println(i+"- "+countArray[i]);
-            countArray[i] = Math.round((countArray[i]*10)/10.0);
-            System.out.println(i+"- "+countArray[i]);
+            //double x = Math.round((countArray[i]*1000)/1000.0);
+            //System.out.println(i+"- "+ x);
 
         }
+        return;
     }
     
     
@@ -74,15 +77,15 @@ public class testing2 extends Application{
 
         XYChart.Series series1 = new XYChart.Series();
         series1.setName("digit frequency");       
-        series1.getData().add(new XYChart.Data(one,2));
-        series1.getData().add(new XYChart.Data(two,4));
-        series1.getData().add(new XYChart.Data(three,6));
-        series1.getData().add(new XYChart.Data(four,8));
-        series1.getData().add(new XYChart.Data(five,10));
-        series1.getData().add(new XYChart.Data(six,9));
-        series1.getData().add(new XYChart.Data(seven,5));
-        series1.getData().add(new XYChart.Data(eight,3));
-        series1.getData().add(new XYChart.Data(nine,6));
+        series1.getData().add(new XYChart.Data(one,countArray[1]));
+        series1.getData().add(new XYChart.Data(two,countArray[2])));
+        series1.getData().add(new XYChart.Data(three,countArray[3])));
+        series1.getData().add(new XYChart.Data(four,countArray[4])));
+        series1.getData().add(new XYChart.Data(five,countArray[5])));
+        series1.getData().add(new XYChart.Data(six,countArray[6])));
+        series1.getData().add(new XYChart.Data(seven,countArray[7])));
+        series1.getData().add(new XYChart.Data(eight,countArray[8])));
+        series1.getData().add(new XYChart.Data(nine,countArray[9])));
 
         Scene scene  = new Scene(bc,800,600);
         bc.getData().addAll(series1);
